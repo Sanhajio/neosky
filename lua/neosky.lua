32181b302b8e3f1f509a940bb9b6b9aabb3c86d7
Sanhaji Omar <sanhaji.omar@gmail.com>
2024-03-18 20:05:57 +0100

    Bridging nvim and bluesky

--- lua/neosky.lua
+++ lua/neosky.lua
@@ -2,8 +2,25 @@ local log = require("plenary.log")
 local M = {}
 
 M.config = {
-	bin = "/home/sanhajio/development/pp/growth/rbsky/target/debug/rbsky-nvim",
+	bin = "/home/sanhajio/development/rbsky/target/debug/rbsky-nvim",
+	bufname = "neosky.social",
 }
+
+M._find_or_create_buffer = function(bufname)
+	-- Check if the buffer already exists
+	for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+		log.info(vim.api.nvim_buf_get_name(bufnr))
+		local suffix = bufname
+		if vim.api.nvim_buf_get_name(bufnr):sub(-#suffix) == suffix then
+			return bufnr
+		end
+	end
+
+	-- Create a new buffer if it doesn't exist
+	local bufnr = vim.api.nvim_create_buf(false, true) -- nomodifiable, not listed
+	vim.api.nvim_buf_set_name(bufnr, bufname)
+	return bufnr
+end
 
 M.content = {}
 
@@ -25,9 +42,41 @@ M.start = function()
 end
 
 M.read = function()
-	local content = {}
-	vim.fn.rpcnotify(M.job_id, "read")
-	log.info(vim.inspect(content))
+	-- vim.fn.rpcnotify(M.job_id, "read")
+	local feed_json = vim.api.nvim_get_var("neosky_feed")
+
+	-- Decode the JSON string to a Lua table
+	local content = vim.fn.json_decode(feed_json)
+
+	-- Optionally log or process the content table
+	-- log.info(vim.inspect(content))
+	if content == nil then
+		log.error("No content to display")
+		return
+	end
+
+	local bufnr = M._find_or_create_buffer("neosky.social")
+	local posts = {}
+	for _, item in ipairs(content) do
+		table.insert(
+			posts,
+			string.format(
+				"%s		@%s		%s",
+				item.post.author.displayName,
+				item.post.author.handle,
+				item.post.record.createdAt
+			)
+		)
+		for line in item.post.record.text:gmatch("([^\n]*)\n?") do
+			table.insert(posts, line)
+		end
+	end
+	vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, posts)
+	vim.api.nvim_set_current_buf(bufnr)
+	vim.api.nvim_win_set_cursor(0, { 1, 0 })
+
+	-- Optionally clear g:neosky_feed after processing
+	-- vim.api.nvim_set_var("neosky_feed", vim.fn.json_encode({}))
 end
 
 M.stop = function()
@@ -36,7 +85,7 @@ M.stop = function()
 end
 
 M.update_feed = function(feed_data)
-	log.debug("Calling Update feed with")
+	log.info(string.format("Calling Update feed with %s", feed_data))
 end
 
 return M
