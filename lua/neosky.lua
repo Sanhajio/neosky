0e8fc891dec94ad6b7c0ba608c0d2a5227cd1b69
Sanhaji Omar <omar.sanhaji@easybourse.com>
2024-03-21 06:51:25 +0100

    Adding response to the buffer

--- lua/neosky.lua
+++ lua/neosky.lua
@@ -40,42 +40,65 @@ M.start = function()
 	end
 end
 
+-- TODO:Add replies, reply counts,
+-- TODO: find a way to keep track of the posts lines, from line 1 -> 10; post did, etc.
 M.read = function()
 	local feed_json = vim.fn.rpcrequest(M.job_id, "read")
-	log.debg(vim.inspect(feed_json))
-
-	-- Decode the JSON string to a Lua table
 	local content = vim.fn.json_decode(feed_json)
 
-	-- Optionally log or process the content table
-	log.info(vim.inspect(content))
 	if content == nil then
 		log.error("No content to display")
 		return
 	end
 
 	local bufnr = M._find_or_create_buffer("neosky.social")
-	log.info("item: ", content)
 	M.posts = {}
+	M.line_to_post_map = {} -- Reset the map for fresh mapping
+
+	local current_line = 1 -- Keep track of the current line number
 	for _, item in ipairs(content) do
-		log.info("item: ", content)
-		table.insert(
-			M.posts,
-			string.format(
-				"%s		@%s		%s",
-				item.post.author.displayName,
-				item.post.author.handle,
-				item.post.record.createdAt
-			)
+		-- Main post
+		local post_info = string.format(
+			"%s\t@%s\t%s",
+			item.post.author.displayName,
+			item.post.author.handle,
+			item.post.record.createdAt
 		)
+		table.insert(M.posts, post_info)
+		M.line_to_post_map[current_line] = item.post.cid
+		current_line = current_line + 1
+
+		-- Post text
 		for line in item.post.record.text:gmatch("([^\n]*)\n?") do
 			table.insert(M.posts, line)
+			M.line_to_post_map[current_line] = item.post.cid
+			current_line = current_line + 1
+		end
+
+		-- Handle replies, if any
+		if item.reply and item.reply.parent then
+			-- Here, we add a tab for indentation to visually represent a reply
+			local reply_info = string.format(
+				"\t↪ %s\t@%s\t%s",
+				item.reply.parent.author.displayName,
+				item.reply.parent.author.handle,
+				item.reply.parent.record.createdAt
+			)
+			table.insert(M.posts, reply_info)
+			M.line_to_post_map[current_line] = item.reply.parent.cid
+			current_line = current_line + 1
+
+			-- Reply text, with indentation
+			for line in item.reply.parent.record.text:gmatch("([^\n]*)\n?") do
+				table.insert(M.posts, "\t" .. line)
+				M.line_to_post_map[current_line] = item.reply.parent.cid
+				current_line = current_line + 1
+			end
 		end
 	end
 	vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, M.posts)
 	vim.api.nvim_set_current_buf(bufnr)
 	vim.api.nvim_win_set_cursor(0, { 1, 0 })
-	--
 end
 
 M.stop = function()
